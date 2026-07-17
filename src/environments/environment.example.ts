fa5f0957ab17f2566b8731a5bec0e4dc5c8cf811
anshuonline <helloanshu.dev@gmail.com>
2026-07-17 10:59:14 +0530

    chore: remove environment secrets from git tracking and add example template

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -2,16 +2,16 @@ export const environment = {
   production: false,
   // ⚠️ Add your YouTube Data API v3 key here.
   // Get one at: https://console.cloud.google.com/apis/library/youtube.googleapis.com
-  youtubeApiKey: 'AIzaSyDsSDmNFF4Z74VTdbaLuAMAw4ivWSFPHYI',
+  youtubeApiKey: 'YOUR_YOUTUBE_API_KEY_HERE',
   youtubeApiUrl: 'https://www.googleapis.com/youtube/v3',
   backendUrl: '/api',
   firebase: {
-    apiKey: "AIzaSyBK4yJx3qsX0h5pCPxTVJgWzLkpcK_anU0",
+    apiKey: "YOUR_FIREBASE_API_KEY_HERE",
     authDomain: "ganatube-8ec4a.firebaseapp.com",
     projectId: "ganatube-8ec4a",
     storageBucket: "ganatube-8ec4a.firebasestorage.app",
-    messagingSenderId: "749322584286",
-    appId: "1:749322584286:web:2ee6b343504918ad9c024f",
-    measurementId: "G-HBKZRGKVH7"
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
   }
 };
